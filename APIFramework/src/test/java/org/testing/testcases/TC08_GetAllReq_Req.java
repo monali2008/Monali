c3cf37c47c6e593600a2d5ecca4745fe0e13727f
pppppppppppppppppppp <pppppppppppppppppppp@LAPTOP-1Q2K33P0>
2021-07-04 17:45:03 +0530

    commiting the changes

--- APIFramework/src/test/java/org/testing/testcases/TC08_GetAllReq_Req.java
+++ APIFramework/src/test/java/org/testing/testcases/TC08_GetAllReq_Req.java
@@ -1,9 +1,11 @@
 package org.testing.testcases;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 import org.testing.teststeps.HTTPMethods;
+import org.testing.utilities.JsonParsingUsingOrgJson;
 import org.testing.utilities.PropertiesFile;
 import org.testng.annotations.Test;
 
@@ -14,12 +16,19 @@ public class TC08_GetAllReq_Req {
 	@Test
 	public void eightTestCase() throws IOException
 	{
+		String extractParameter="id";
+		
 		Properties pr= PropertiesFile.readProperties("../APIFramework/URI.properties");
 		HTTPMethods hr=new HTTPMethods(pr);
 		Response rs=hr.GetRequestQueryParameter("REQ_URI", "page=2");
 		
 		System.out.println("Status code of Get all req request ia: "+rs.statusCode());
 		System.out.println("Body of Get all req is: "+rs.asString());
+		
+		List<Object>ob=JsonParsingUsingOrgJson.parseJSONArray(rs, "data", extractParameter);
+		for(Object o:ob) {
+			System.out.println(extractParameter+" is: "+"'"+o+"'");
+		}
 		System.out.println("----------------------------------------------");
 	}
 
--- APIFramework/src/test/java/org/testing/utilities/JsonParsingUsingOrgJson.java
+++ APIFramework/src/test/java/org/testing/utilities/JsonParsingUsingOrgJson.java
@@ -1,6 +1,14 @@
 package org.testing.utilities;
 
+import java.util.ArrayList;
+import java.util.List;
+import static io.restassured.RestAssured.*;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import io.restassured.response.Response;
+
 
 public class JsonParsingUsingOrgJson {
 	
@@ -12,6 +20,22 @@ public class JsonParsingUsingOrgJson {
 			return js.getString(extractKey);
 		}
 		return extractKey;
+		
+	}
+	
+	public static List<Object>parseJSONArray(Response res,String key,String extractKey)
+	{
+		
+		List<Object>list=new ArrayList<Object>();
+		JSONObject js=new JSONObject(res.asString());
+		JSONArray arr=js.getJSONArray(key);
+		for(int i=0;i<arr.length();i++)
+		{
+			JSONObject js1=arr.getJSONObject(i);
+			Object js2=js1.get(extractKey);
+			list.add(js2);
+		}
+		return list;
 		
 	}
 
